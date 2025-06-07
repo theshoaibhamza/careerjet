acfde1d550b690c7cdc0e9172f9b2c577ce05019
Shoaib hamza <bigbrains997@gmail.com>
2025-06-06 17:22:57 -0700

    asdasd

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, jsonify # type: ignore
 import requests
 
@@ -37,5 +36,4 @@ def search_jobs():
     return jsonify({'success': True, 'jobs': jobs})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
